1a6b3bdee94e9bb5331c466714cdd39c1c96c1a3
Jake Allen <jake@hintsy.co>
2021-10-11 09:17:33 -0700

    It's alive. Can now mint, view tokenURI, etc.

--- contracts/NounsToken.sol
+++ contracts/NounsToken.sol
@@ -36,8 +36,8 @@ contract NounsToken is INounsToken, Ownable, ERC721Enumerable {
     uint256 public max_tokens = 1000;
     uint256 public mint_limit= 20;
 
-    // Hat svg
-    string public hatSvg = 'PHBhdGggZD0iTTIxMCA3MEg1MFY4MEgyMTBWNzBaIiBmaWxsPSIjRTExODMzIi8+CjxwYXRoIGQ9Ik0xODAgNTBINjBWNjBIMTgwVjUwWiIgZmlsbD0iI0UxMTgzMyIvPgo8cGF0aCBkPSJNMTcwIDQwSDcwVjUwSDE3MFY0MFoiIGZpbGw9IiNFMTE4MzMiLz4KPHBhdGggZD0iTTE3MCAzMEg3MFY0MEgxNzBWMzBaIiBmaWxsPSIjRTExODMzIi8+CjxyZWN0IHdpZHRoPSIxMzAiIGhlaWdodD0iMTAiIHRyYW5zZm9ybT0ibWF0cml4KC0xIDAgMCAxIDE4MCA2MCkiIGZpbGw9IiNCRDJEMjQiLz4KPHBhdGggZD0iTTEyMCA3MEgxMTBWODBIMTIwVjcwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTIwIDcwSDExMFY4MEgxMjBWNzBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xMjAgNzBIMTEwVjgwSDEyMFY3MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTEyMCA3MEgxMTBWODBIMTIwVjcwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTIwIDcwSDExMFY4MEgxMjBWNzBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xMjAgNzBIMTEwVjgwSDEyMFY3MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTEyMCA3MEgxMTBWODBIMTIwVjcwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTIwIDcwSDExMFY4MEgxMjBWNzBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xMjAgNzBIMTEwVjgwSDEyMFY3MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTEyMCA3MEgxMTBWODBIMTIwVjcwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTIwIDYwSDExMFY3MEgxMjBWNjBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xMjAgNjBIMTEwVjcwSDEyMFY2MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTEyMCA2MEgxMTBWNzBIMTIwVjYwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTIwIDYwSDExMFY3MEgxMjBWNjBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xMjAgNjBIMTEwVjcwSDEyMFY2MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTEyMCA2MEgxMTBWNzBIMTIwVjYwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTIwIDYwSDExMFY3MEgxMjBWNjBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xMjAgNjBIMTEwVjcwSDEyMFY2MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTEyMCA2MEgxMTBWNzBIMTIwVjYwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTIwIDYwSDExMFY3MEgxMjBWNjBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xMzAgNTBIMTIwVjYwSDEzMFY1MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTEzMCA1MEgxMjBWNjBIMTMwVjUwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTMwIDUwSDEyMFY2MEgxMzBWNTBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xMzAgNTBIMTIwVjYwSDEzMFY1MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTEzMCA1MEgxMjBWNjBIMTMwVjUwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTMwIDUwSDEyMFY2MEgxMzBWNTBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xMzAgNTBIMTIwVjYwSDEzMFY1MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTEzMCA1MEgxMjBWNjBIMTMwVjUwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTMwIDUwSDEyMFY2MEgxMzBWNTBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xMzAgNTBIMTIwVjYwSDEzMFY1MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTE1MCA1MEgxNDBWNjBIMTUwVjUwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTUwIDUwSDE0MFY2MEgxNTBWNTBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xNTAgNTBIMTQwVjYwSDE1MFY1MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTE1MCA1MEgxNDBWNjBIMTUwVjUwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTUwIDUwSDE0MFY2MEgxNTBWNTBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xNTAgNTBIMTQwVjYwSDE1MFY1MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTE1MCA1MEgxNDBWNjBIMTUwVjUwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTUwIDUwSDE0MFY2MEgxNTBWNTBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xNTAgNTBIMTQwVjYwSDE1MFY1MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTE1MCA1MEgxNDBWNjBIMTUwVjUwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTYwIDYwSDE1MFY3MEgxNjBWNjBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xNjAgNjBIMTUwVjcwSDE2MFY2MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTE2MCA2MEgxNTBWNzBIMTYwVjYwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTYwIDYwSDE1MFY3MEgxNjBWNjBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xNjAgNjBIMTUwVjcwSDE2MFY2MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTE2MCA2MEgxNTBWNzBIMTYwVjYwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTYwIDYwSDE1MFY3MEgxNjBWNjBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xNjAgNjBIMTUwVjcwSDE2MFY2MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTE2MCA2MEgxNTBWNzBIMTYwVjYwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTYwIDYwSDE1MFY3MEgxNjBWNjBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xNjAgNzBIMTUwVjgwSDE2MFY3MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTE2MCA3MEgxNTBWODBIMTYwVjcwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTYwIDcwSDE1MFY4MEgxNjBWNzBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xNjAgNzBIMTUwVjgwSDE2MFY3MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTE2MCA3MEgxNTBWODBIMTYwVjcwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTYwIDcwSDE1MFY4MEgxNjBWNzBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xNjAgNzBIMTUwVjgwSDE2MFY3MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTE2MCA3MEgxNTBWODBIMTYwVjcwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTYwIDcwSDE1MFY4MEgxNjBWNzBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xNjAgNzBIMTUwVjgwSDE2MFY3MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTE1MCA3MEgxNDBWODBIMTUwVjcwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTUwIDcwSDE0MFY4MEgxNTBWNzBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xNTAgNzBIMTQwVjgwSDE1MFY3MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTE1MCA3MEgxNDBWODBIMTUwVjcwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTUwIDcwSDE0MFY4MEgxNTBWNzBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xNTAgNzBIMTQwVjgwSDE1MFY3MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTE1MCA3MEgxNDBWODBIMTUwVjcwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTUwIDcwSDE0MFY4MEgxNTBWNzBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xNTAgNzBIMTQwVjgwSDE1MFY3MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTE1MCA3MEgxNDBWODBIMTUwVjcwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTQwIDcwSDEzMFY4MEgxNDBWNzBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xNDAgNzBIMTMwVjgwSDE0MFY3MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTE0MCA3MEgxMzBWODBIMTQwVjcwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTQwIDcwSDEzMFY4MEgxNDBWNzBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xNDAgNzBIMTMwVjgwSDE0MFY3MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTE0MCA3MEgxMzBWODBIMTQwVjcwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTQwIDcwSDEzMFY4MEgxNDBWNzBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xNDAgNzBIMTMwVjgwSDE0MFY3MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTE0MCA3MEgxMzBWODBIMTQwVjcwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTQwIDcwSDEzMFY4MEgxNDBWNzBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xMzAgNzBIMTIwVjgwSDEzMFY3MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTEzMCA3MEgxMjBWODBIMTMwVjcwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTMwIDcwSDEyMFY4MEgxMzBWNzBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xMzAgNzBIMTIwVjgwSDEzMFY3MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTEzMCA3MEgxMjBWODBIMTMwVjcwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTMwIDcwSDEyMFY4MEgxMzBWNzBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xMzAgNzBIMTIwVjgwSDEzMFY3MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTEzMCA3MEgxMjBWODBIMTMwVjcwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTMwIDcwSDEyMFY4MEgxMzBWNzBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xMzAgNzBIMTIwVjgwSDEzMFY3MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTE0MCA2MEgxMzBWNzBIMTQwVjYwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTQwIDYwSDEzMFY3MEgxNDBWNjBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xNDAgNjBIMTMwVjcwSDE0MFY2MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTE0MCA2MEgxMzBWNzBIMTQwVjYwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTQwIDYwSDEzMFY3MEgxNDBWNjBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xNDAgNjBIMTMwVjcwSDE0MFY2MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTE0MCA2MEgxMzBWNzBIMTQwVjYwWiIgZmlsbD0iI0VFRDgxMSIvPgo8cGF0aCBkPSJNMTQwIDYwSDEzMFY3MEgxNDBWNjBaIiBmaWxsPSIjRUVEODExIi8+CjxwYXRoIGQ9Ik0xNDAgNjBIMTMwVjcwSDE0MFY2MFoiIGZpbGw9IiNFRUQ4MTEiLz4KPHBhdGggZD0iTTE0MCA2MEgxMzBWNzBIMTQwVjYwWiIgZmlsbD0iI0VFRDgxMSIvPg';
+    // Hat & opening SVG tag
+    string public hatSVG = '<rect width="160" height="10" transform="matrix(-1 0 0 1 240 80)" fill="#E11833"/><rect width="120" height="10" transform="matrix(-1 0 0 1 210 60)" fill="#E11833"/><rect width="100" height="10" transform="matrix(-1 0 0 1 200 50)" fill="#E11833"/><rect width="100" height="10" transform="matrix(-1 0 0 1 200 40)" fill="#E11833"/><rect width="130" height="10" transform="matrix(-1 0 0 1 210 70)" fill="#BD2D24"/><rect width="50" height="10" transform="matrix(-1 0 0 1 190 70)" fill="#EED811"/><rect width="10" height="10" transform="matrix(-1 0 0 1 150 60)" fill="#EED811"/><rect width="10" height="10" transform="matrix(-1 0 0 1 160 50)" fill="#EED811"/><rect width="10" height="10" transform="matrix(-1 0 0 1 170 60)" fill="#EED811"/><rect width="10" height="10" transform="matrix(-1 0 0 1 180 50)" fill="#EED811"/><rect width="10" height="10" transform="matrix(-1 0 0 1 190 60)" fill="#EED811"/>';
 
     // Store custom descriptions for GOOPs
     mapping (uint => string) public customDescription;
@@ -117,12 +117,9 @@ contract NounsToken is INounsToken, Ownable, ERC721Enumerable {
         return description;
     }
 
-    constructor(address _descriptor) ERC721('Fast Food Nouns', 'FFN') {
-        // We're using our own descriptor. That contract is deployed, then the
-        // address is passed here as a param.
-        descriptor = INounsDescriptor(_descriptor);
-
+    constructor() ERC721('Fast Food Nouns', 'FFN') {
         // MAINNET
+        descriptor = INounsDescriptor(0x0Cfdb3Ba1694c2bb2CFACB0339ad7b1Ae5932B63);
         seeder = INounsSeeder(0xCC8a0FB5ab3C7132c1b2A0109142Fb112c4Ce515);
         // RINKEBY
         // seeder = INounsSeeder(0xA98A1b1Cc4f5746A753167BAf8e0C26AcBe42F2E);
@@ -196,7 +193,19 @@ contract NounsToken is INounsToken, Ownable, ERC721Enumerable {
         string memory NounID = tokenId.toString();
         string memory name = string(abi.encodePacked('Fast Food Noun ', NounID)); 
         string memory description = viewDescription(tokenId);
-        return descriptor.genericDataURI(name, description, seeds[tokenId]);
+        // MODIFY THE SVG
+        string memory SVG = descriptor.generateSVGImage(seeds[tokenId]);
+        // decode base64 SVG into bytes
+        bytes memory decodedSVG = Base64.decode(SVG);
+        // remove the SVG closing tag `</svg>`
+        string memory substring = removeLastSVGTag(decodedSVG);
+        // concatenate existing SVG (minus closing tag), hat, and closing tag
+        string memory finalSVG = string(abi.encodePacked(substring, hatSVG, '</svg>'));
+        // rencode 
+        string memory encodedFinalSVG = Base64.encode(bytes(finalSVG));
+        // compose json
+        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "', name, '", "description": "', description, '", "image": "data:image/svg+xml;base64,', encodedFinalSVG, '"}'))));
+        return string(abi.encodePacked('data:application/json;base64,', json));
     }
 
     /**
@@ -243,19 +252,153 @@ contract NounsToken is INounsToken, Ownable, ERC721Enumerable {
      * @notice Mint a Noun with `nounId` to the provided `to` address.
      */
     function _mintTo(address to, uint256 nounId) internal returns (uint256) {
-        // IMPORTANT: Must be the Nouns descriptor, not the Fast Food
-        // descriptor, because the `generateSeed` function counts the available
-        // body part types before determining random seeds for them.
-        // MAINNET
-        INounsDescriptor nounsDescriptor = INounsDescriptor(0x0Cfdb3Ba1694c2bb2CFACB0339ad7b1Ae5932B63);
-        // RINKEBY
-        // INounsDescriptor nounsDescriptor = INounsDescriptor(0x53cB482c73655D2287AE3282AD1395F82e6a402F);
-
-        INounsSeeder.Seed memory seed = seeds[nounId] = seeder.generateSeed(nounId, nounsDescriptor);
+        INounsSeeder.Seed memory seed = seeds[nounId] = seeder.generateSeed(nounId, descriptor);
         _mint(to, nounId);
         emit NounCreated(nounId, seed);
 
         return nounId;
     }
 
+    // Returns SVG string without the closing tag so we can insert more elements
+    function removeLastSVGTag(bytes memory svgBytes) internal pure returns (string memory) {
+        bytes memory result = svgBytes;        
+        // NOTE: `result` remains the same length as `svgBytes`, but abi.encode
+        // seems to ignore the empty bytes at the end of result. so choosing this
+        // route over a for loop bc it seems more gas efficient.
+        delete result[result.length - 1];
+        delete result[result.length - 2];
+        delete result[result.length - 3];
+        delete result[result.length - 4];
+        delete result[result.length - 5];
+        delete result[result.length - 6];
+        return string(result);
+    }
+
+}
+
+/// @title Base64
+/// @author Brecht Devos - <brecht@loopring.org>
+/// @notice Provides functions for encoding/decoding base64
+library Base64 {
+    string internal constant TABLE_ENCODE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+    bytes  internal constant TABLE_DECODE = hex"0000000000000000000000000000000000000000000000000000000000000000"
+                                            hex"00000000000000000000003e0000003f3435363738393a3b3c3d000000000000"
+                                            hex"00000102030405060708090a0b0c0d0e0f101112131415161718190000000000"
+                                            hex"001a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132330000000000";
+
+    function encode(bytes memory data) internal pure returns (string memory) {
+        if (data.length == 0) return '';
+
+        // load the table into memory
+        string memory table = TABLE_ENCODE;
+
+        // multiply by 4/3 rounded up
+        uint256 encodedLen = 4 * ((data.length + 2) / 3);
+
+        // add some extra buffer at the end required for the writing
+        string memory result = new string(encodedLen + 32);
+
+        assembly {
+            // set the actual output length
+            mstore(result, encodedLen)
+
+            // prepare the lookup table
+            let tablePtr := add(table, 1)
+
+            // input ptr
+            let dataPtr := data
+            let endPtr := add(dataPtr, mload(data))
+
+            // result ptr, jump over length
+            let resultPtr := add(result, 32)
+
+            // run over the input, 3 bytes at a time
+            for {} lt(dataPtr, endPtr) {}
+            {
+                // read 3 bytes
+                dataPtr := add(dataPtr, 3)
+                let input := mload(dataPtr)
+
+                // write 4 characters
+                mstore8(resultPtr, mload(add(tablePtr, and(shr(18, input), 0x3F))))
+                resultPtr := add(resultPtr, 1)
+                mstore8(resultPtr, mload(add(tablePtr, and(shr(12, input), 0x3F))))
+                resultPtr := add(resultPtr, 1)
+                mstore8(resultPtr, mload(add(tablePtr, and(shr( 6, input), 0x3F))))
+                resultPtr := add(resultPtr, 1)
+                mstore8(resultPtr, mload(add(tablePtr, and(        input,  0x3F))))
+                resultPtr := add(resultPtr, 1)
+            }
+
+            // padding with '='
+            switch mod(mload(data), 3)
+            case 1 { mstore(sub(resultPtr, 2), shl(240, 0x3d3d)) }
+            case 2 { mstore(sub(resultPtr, 1), shl(248, 0x3d)) }
+        }
+
+        return result;
+    }
+
+    function decode(string memory _data) internal pure returns (bytes memory) {
+        bytes memory data = bytes(_data);
+
+        if (data.length == 0) return new bytes(0);
+        require(data.length % 4 == 0, "invalid base64 decoder input");
+
+        // load the table into memory
+        bytes memory table = TABLE_DECODE;
+
+        // every 4 characters represent 3 bytes
+        uint256 decodedLen = (data.length / 4) * 3;
+
+        // add some extra buffer at the end required for the writing
+        bytes memory result = new bytes(decodedLen + 32);
+
+        assembly {
+            // padding with '='
+            let lastBytes := mload(add(data, mload(data)))
+            if eq(and(lastBytes, 0xFF), 0x3d) {
+                decodedLen := sub(decodedLen, 1)
+                if eq(and(lastBytes, 0xFFFF), 0x3d3d) {
+                    decodedLen := sub(decodedLen, 1)
+                }
+            }
+
+            // set the actual output length
+            mstore(result, decodedLen)
+
+            // prepare the lookup table
+            let tablePtr := add(table, 1)
+
+            // input ptr
+            let dataPtr := data
+            let endPtr := add(dataPtr, mload(data))
+
+            // result ptr, jump over length
+            let resultPtr := add(result, 32)
+
+            // run over the input, 4 characters at a time
+            for {} lt(dataPtr, endPtr) {}
+            {
+               // read 4 characters
+               dataPtr := add(dataPtr, 4)
+               let input := mload(dataPtr)
+
+               // write 3 bytes
+               let output := add(
+                   add(
+                       shl(18, and(mload(add(tablePtr, and(shr(24, input), 0xFF))), 0xFF)),
+                       shl(12, and(mload(add(tablePtr, and(shr(16, input), 0xFF))), 0xFF))),
+                   add(
+                       shl( 6, and(mload(add(tablePtr, and(shr( 8, input), 0xFF))), 0xFF)),
+                               and(mload(add(tablePtr, and(        input , 0xFF))), 0xFF)
+                    )
+                )
+                mstore(resultPtr, shl(232, output))
+                resultPtr := add(resultPtr, 3)
+            }
+        }
+
+        return result;
+    }
 }
