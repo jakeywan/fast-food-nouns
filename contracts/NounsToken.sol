1dd9e1887e3768ec472efc36d0783257a33c4f6b
Jake Allen <jake@hintsy.co>
2021-10-11 21:30:53 -0700

    Added ability to adjust clothes, add new clothes, etc.

--- contracts/NounsToken.sol
+++ contracts/NounsToken.sol
@@ -31,16 +31,22 @@ import 'hardhat/console.sol';
 contract NounsToken is INounsToken, Ownable, ERC721Enumerable {
     using Strings for uint256;
 
-    // Price and maximum number of Fast Food Nouns
     uint256 public price = 30000000000000000;
     uint256 public max_tokens = 1000;
     uint256 public mint_limit= 20;
 
-    // Hat SVG elements
     string public hatSVG = '<rect width="160" height="10" x="80" y="80" fill="#E11833"/><rect width="120" height="10" x="90" y="60" fill="#E11833"/><rect width="100" height="10" x="100" y="50" fill="#E11833"/><rect width="100" height="10" x="100" y="40" fill="#E11833"/><rect width="130" height="10" x="80" y="70" fill="#BD2D24"/><rect width="50" height="10" x="140" y="70" fill="#EED811"/><rect width="10" height="10" x="140" y="60" fill="#EED811"/><rect width="10" height="10" x="150" y="50" fill="#EED811"/><rect width="10" height="10" x="160" y="60" fill="#EED811"/><rect width="10" height="10" x="170" y="50" fill="#EED811"/><rect width="10" height="10" x="180" y="60" fill="#EED811"/>';
 
-    // Store custom descriptions for GOOPs
-    mapping (uint => string) public customDescription;
+    // An array of publicly available clothes (SVG snippets)
+    string[] public clothingList;
+
+    // Tracks state of clothing per tokenId. Example: Noun #0 is wearing 3 items
+    // of clothing at `clothingList` index 4, 9, and 14. This will represented
+    // as `clothingState[0][4,9,14]`.
+    mapping (uint256 => uint256[]) public clothingState;
+
+    // Store custom descriptions for fast food nouns
+    mapping (uint256 => string) public customDescription;
 
     // The Nouns token URI descriptor
     INounsDescriptor public descriptor;
@@ -117,7 +123,35 @@ contract NounsToken is INounsToken, Ownable, ERC721Enumerable {
         return description;
     }
 
+    // Specifies clothes owner would like to wear. Overwrites existing selections,
+    // so always include every items you'd like to wear.
+    function wearClothes(uint256 tokenId, uint256[] calldata clothes) public returns (string memory) {
+        require (msg.sender == ownerOf(tokenId), "not your Noun");
+        clothingState[tokenId] = clothes;
+        return 'Updated your clothing';
+    }
+
+    // Add clothes to the list of publicly available clothes
+    function addClothes(string calldata svgSnippet) public onlyOwner returns (string memory) {
+        clothingList.push(svgSnippet);
+        return 'Updated clothing list';
+    }
+
+    // Replace clothing at a specific index
+    function setClothesAtIndex(uint256 index, string calldata svgSnippet) public onlyOwner returns (string memory) {
+        clothingList[index] = svgSnippet;
+        return 'Updated clothing list';
+    }
+
+    // Return the list of clothes selected for a given tokenId
+    function getClothesForTokenId(uint256 tokenId) public view returns (uint[] memory) {
+        return clothingState[tokenId];
+    }
+
     constructor() ERC721('Fast Food Nouns', 'FFN') {
+        // Populate `clothingList` with fast food hat
+        clothingList.push(hatSVG);
+
         // MAINNET
         // descriptor = INounsDescriptor(0x0Cfdb3Ba1694c2bb2CFACB0339ad7b1Ae5932B63);
         // seeder = INounsSeeder(0xCC8a0FB5ab3C7132c1b2A0109142Fb112c4Ce515);
@@ -195,17 +229,26 @@ contract NounsToken is INounsToken, Ownable, ERC721Enumerable {
         string memory NounID = tokenId.toString();
         string memory name = string(abi.encodePacked('Fast Food Noun ', NounID)); 
         string memory description = viewDescription(tokenId);
-        // MODIFY THE SVG
+
+        // Fetch the original SVG (base64 encoded) from Nouns descriptor
         string memory SVG = descriptor.generateSVGImage(seeds[tokenId]);
-        // decode base64 SVG returned from Nouns contract into bytes
+        // Decode base64 SVG into bytes
         bytes memory decodedSVG = Base64.decode(SVG);
-        // remove the SVG closing tag `</svg>`
+        // Remove the SVG closing tag `</svg>`
         string memory substring = removeLastSVGTag(decodedSVG);
-        // concatenate retrieved SVG (minus closing tag), our hat, and new closing tag
-        string memory finalSVG = string(abi.encodePacked(substring, hatSVG, '</svg>'));
-        // rencode SVG to base64
-        string memory encodedFinalSVG = Base64.encode(bytes(finalSVG));
-        // compose json string
+        // Encode substring bytes
+        bytes memory finalSVGBytes = abi.encodePacked(substring);
+        // Loop through clothes for this tokenId and encode them in
+        for (uint256 i = 0; i < clothingState[tokenId].length; i++) {
+            uint256 clothingIndexToAdd = clothingState[tokenId][i];
+            // Encode the clothing bytes for these clothes
+            finalSVGBytes = abi.encodePacked(finalSVGBytes, clothingList[clothingIndexToAdd]);
+        }
+        // Finally, encode the closing SVG tag in
+        finalSVGBytes = abi.encodePacked(finalSVGBytes, '</svg>');
+        // Rencode SVG to base64
+        string memory encodedFinalSVG = Base64.encode(finalSVGBytes);
+        // Compose json string
         string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name":"', name, '", "description":"', description, '", "image":"data:image/svg+xml;base64,', encodedFinalSVG, '"}'))));
         return string(abi.encodePacked('data:application/json;base64,', json));
     }
@@ -256,6 +299,8 @@ contract NounsToken is INounsToken, Ownable, ERC721Enumerable {
     function _mintTo(address to, uint256 nounId) internal returns (uint256) {
         INounsSeeder.Seed memory seed = seeds[nounId] = seeder.generateSeed(nounId, descriptor);
         _mint(to, nounId);
+        // Automatically set new mints as wearing the fast food hat
+        clothingState[nounId] = [0];
         emit NounCreated(nounId, seed);
 
         return nounId;
@@ -263,7 +308,7 @@ contract NounsToken is INounsToken, Ownable, ERC721Enumerable {
 
     // Returns SVG string without the closing tag so we can insert more elements
     function removeLastSVGTag(bytes memory svgBytes) internal pure returns (string memory) {
-        // this will be length of svgBytes minus length of svg closing tag `</svg>`
+        // This will be length of svgBytes minus length of svg closing tag `</svg>`
         bytes memory result = new bytes(svgBytes.length - 6);
         for(uint i = 0; i < result.length; i++) {
             result[i] = svgBytes[i];
--- scripts/hardhatDeploy.js
+++ scripts/hardhatDeploy.js
@@ -14,13 +14,27 @@ async function main() {
     gasPrice: 10000000000,
     value: '30000000000000000'
   })
-
   console.log('Minted')
 
-  // Fetch test tokenURI
-  const tokenURI = await nounsToken.tokenURI('0')
+  // set a new clothing item to the closet
+  const clothing = await nounsToken.addClothes('<path d="M230 220H90V230H230V220Z" fill="#005A9C"/><path d="M230 230H90V240H230V230Z" fill="#005A9C"/><path d="M230 240H90V250H230V240Z" fill="#005A9C"/><path d="M110 250H90V260H110V250Z" fill="#005A9C"/><path d="M230 250H120V260H230V250Z" fill="#005A9C"/><path d="M110 260H90V270H110V260Z" fill="#005A9C"/><path d="M230 260H120V270H230V260Z" fill="#005A9C"/><path d="M110 270H90V280H110V270Z" fill="#005A9C"/><path d="M230 270H120V280H230V270Z" fill="#005A9C"/><path d="M110 280H90V290H110V280Z" fill="#005A9C"/><path d="M230 280H120V290H230V280Z" fill="#005A9C"/><path d="M110 290H90V300H110V290Z" fill="#005A9C"/><path d="M230 290H120V300H230V290Z" fill="#005A9C"/><path d="M110 300H90V310H110V300Z" fill="#005A9C"/><path d="M230 300H120V310H230V300Z" fill="#005A9C"/><path d="M110 310H90V320H110V310Z" fill="#005A9C"/><path d="M230 310H120V320H230V310Z" fill="#005A9C"/><path d="M160 230H150V280H160V230Z" fill="white"/><path d="M190 270H160V280H190V270Z" fill="white"/><path d="M180 260H170V300H180V260Z" fill="white"/><path d="M200 260H190V300H200V260Z" fill="white"/><path d="M190 250H180V260H190V250Z" fill="white"/><path d="M230 210H90V220H230V210Z" fill="#005A9C"/>', {
+    from: testAccounts[0]
+  })
+  console.log('Added Dodgers shirt')
 
-  console.log('token uri', tokenURI)
+  const wearClothes = await nounsToken.wearClothes(0, [0, 1], {
+    from: testAccounts[0]
+  })
+  console.log('Wearing shirt')
+
+  const ourClothes = await nounsToken.getClothesForTokenId(0, {
+    from: testAccounts[0]
+  })
+  console.log('Here is our clothes: ', ourClothes)
+
+  // Fetch test tokenURI
+  // const tokenURI = await nounsToken.tokenURI('0')
+  // console.log('token uri', tokenURI)
 
 }
 
