c15a193f3d33c60da0fc3732e968fd827d9bee22
Jake Allen <jake@hintsy.co>
2021-10-18 07:30:45 -0700

    Removed unnecessary parts of INounsDescriptor interface to reduce code size and unused functions/state.

--- contracts/FFNDescriptor.sol
+++ contracts/FFNDescriptor.sol
@@ -1,13 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
 /// @title A custom Fast Food Nouns version of the Nouns NFT descriptor
-/// Modifications of the original:
-/// 1. We're referencing the deployed INounsDescriptor so that we can pull SVG
-/// parts from their (otherwise we'd have to populate our own again).
-/// 2. For body, accessories, etc. we're referencing the deployed descriptor
-/// instead of our own internal state.
-/// 3. We included the referenced libraries in the same file. Easier than linking.
-/// 4. We left the unused functions so that this contract conforms to the interface
 
 /*********************************
  * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
@@ -30,7 +23,7 @@ import { INounsSeeder } from './interfaces/INounsSeeder.sol';
 import { Base64 } from 'base64-sol/base64.sol';
 import 'hardhat/console.sol';
 
-contract FFNDescriptor is INounsDescriptor, Ownable {
+contract FFNDescriptor is Ownable {
     using Strings for uint256;
 
     // modified. the NounsDescriptor contract. For the main Nouns SVG parts, we're going
@@ -43,32 +36,8 @@ contract FFNDescriptor is INounsDescriptor, Ownable {
     // https://creativecommons.org/publicdomain/zero/1.0/legalcode.txt
     bytes32 constant COPYRIGHT_CC0_1_0_UNIVERSAL_LICENSE = 0xa2010f343487d3f7618affe54f789f5487602331c0a8d03f49e9a7c547cf0499;
 
-    // Whether or not new Noun parts can be added
-    bool public override arePartsLocked;
-
-    // Whether or not `tokenURI` should be returned as a data URI (Default: true)
-    bool public override isDataURIEnabled = true;
-
-    // Base URI
-    string public override baseURI;
-
     // Noun Color Palettes (Index => Hex Colors)
-    mapping(uint8 => string[]) public override palettes;
-
-    // Noun Backgrounds (Hex Colors)
-    string[] public override backgrounds;
-
-    // Noun Bodies (Custom RLE)
-    bytes[] public override bodies;
-
-    // Noun Accessories (Custom RLE)
-    bytes[] public override accessories;
-
-    // Noun Heads (Custom RLE)
-    bytes[] public override heads;
-
-    // Noun Glasses (Custom RLE)
-    bytes[] public override glasses;
+    mapping(uint8 => string[]) public palettes;
 
     // Custom Backgrounds (RLE)
     bytes[] public customBackgrounds;
@@ -89,14 +58,6 @@ contract FFNDescriptor is INounsDescriptor, Ownable {
     bytes[] public customOverlays;
 
     /**
-     * @notice Require that the parts have not been locked.
-     */
-    modifier whenPartsNotLocked() {
-        require(!arePartsLocked, 'Parts are locked');
-        _;
-    }
-
-    /**
      * @notice Update the underlying Noun descriptor (in case they change it).
      */
     event DescriptorUpdated(INounsDescriptor descriptor);
@@ -104,41 +65,6 @@ contract FFNDescriptor is INounsDescriptor, Ownable {
         nounDescriptor = _descriptor;
 
         emit DescriptorUpdated(_descriptor);
-    }
-
-    /**
-     * @notice Get the number of available Noun `backgrounds`.
-     */
-    function backgroundCount() external view override returns (uint256) {
-        return backgrounds.length;
-    }
-
-    /**
-     * @notice Get the number of available Noun `bodies`.
-     */
-    function bodyCount() external view override returns (uint256) {
-        return bodies.length;
-    }
-
-    /**
-     * @notice Get the number of available Noun `accessories`.
-     */
-    function accessoryCount() external view override returns (uint256) {
-        return accessories.length;
-    }
-
-    /**
-     * @notice Get the number of available Noun `heads`.
-     */
-    function headCount() external view override returns (uint256) {
-        return heads.length;
-    }
-
-    /**
-     * @notice Get the number of available Noun `glasses`.
-     */
-    function glassesCount() external view override returns (uint256) {
-        return glasses.length;
     }
 
     /**
@@ -187,60 +113,10 @@ contract FFNDescriptor is INounsDescriptor, Ownable {
      * @notice Add colors to a color palette.
      * @dev This function can only be called by the owner.
      */
-    function addManyColorsToPalette(uint8 paletteIndex, string[] calldata newColors) external override onlyOwner {
+    function addManyColorsToPalette(uint8 paletteIndex, string[] calldata newColors) external onlyOwner {
         require(palettes[paletteIndex].length + newColors.length <= 256, 'Palettes can only hold 256 colors');
         for (uint256 i = 0; i < newColors.length; i++) {
             _addColorToPalette(paletteIndex, newColors[i]);
-        }
-    }
-
-    /**
-     * @notice Batch add Noun backgrounds.
-     * @dev This function can only be called by the owner when not locked.
-     */
-    function addManyBackgrounds(string[] calldata _backgrounds) external override onlyOwner whenPartsNotLocked {
-        for (uint256 i = 0; i < _backgrounds.length; i++) {
-            _addBackground(_backgrounds[i]);
-        }
-    }
-
-    /**
-     * @notice Batch add Noun bodies.
-     * @dev This function can only be called by the owner when not locked.
-     */
-    function addManyBodies(bytes[] calldata _bodies) external override onlyOwner whenPartsNotLocked {
-        for (uint256 i = 0; i < _bodies.length; i++) {
-            _addBody(_bodies[i]);
-        }
-    }
-
-    /**
-     * @notice Batch add Noun accessories.
-     * @dev This function can only be called by the owner when not locked.
-     */
-    function addManyAccessories(bytes[] calldata _accessories) external override onlyOwner whenPartsNotLocked {
-        for (uint256 i = 0; i < _accessories.length; i++) {
-            _addAccessory(_accessories[i]);
-        }
-    }
-
-    /**
-     * @notice Batch add Noun heads.
-     * @dev This function can only be called by the owner when not locked.
-     */
-    function addManyHeads(bytes[] calldata _heads) external override onlyOwner whenPartsNotLocked {
-        for (uint256 i = 0; i < _heads.length; i++) {
-            _addHead(_heads[i]);
-        }
-    }
-
-    /**
-     * @notice Batch add Noun glasses.
-     * @dev This function can only be called by the owner when not locked.
-     */
-    function addManyGlasses(bytes[] calldata _glasses) external override onlyOwner whenPartsNotLocked {
-        for (uint256 i = 0; i < _glasses.length; i++) {
-            _addGlasses(_glasses[i]);
         }
     }
 
@@ -302,49 +178,9 @@ contract FFNDescriptor is INounsDescriptor, Ownable {
      * @notice Add a single color to a color palette.
      * @dev This function can only be called by the owner.
      */
-    function addColorToPalette(uint8 _paletteIndex, string calldata _color) external override onlyOwner {
+    function addColorToPalette(uint8 _paletteIndex, string calldata _color) external onlyOwner {
         require(palettes[_paletteIndex].length <= 255, 'Palettes can only hold 256 colors');
         _addColorToPalette(_paletteIndex, _color);
-    }
-
-    /**
-     * @notice Add a Noun background.
-     * @dev This function can only be called by the owner when not locked.
-     */
-    function addBackground(string calldata _background) external override onlyOwner whenPartsNotLocked {
-        _addBackground(_background);
-    }
-
-    /**
-     * @notice Add a Noun body.
-     * @dev This function can only be called by the owner when not locked.
-     */
-    function addBody(bytes calldata _body) external override onlyOwner whenPartsNotLocked {
-        _addBody(_body);
-    }
-
-    /**
-     * @notice Add a Noun accessory.
-     * @dev This function can only be called by the owner when not locked.
-     */
-    function addAccessory(bytes calldata _accessory) external override onlyOwner whenPartsNotLocked {
-        _addAccessory(_accessory);
-    }
-
-    /**
-     * @notice Add a Noun head.
-     * @dev This function can only be called by the owner when not locked.
-     */
-    function addHead(bytes calldata _head) external override onlyOwner whenPartsNotLocked {
-        _addHead(_head);
-    }
-
-    /**
-     * @notice Add Noun glasses.
-     * @dev This function can only be called by the owner when not locked.
-     */
-    function addGlasses(bytes calldata _glasses) external override onlyOwner whenPartsNotLocked {
-        _addGlasses(_glasses);
     }
 
     /**
@@ -390,54 +226,17 @@ contract FFNDescriptor is INounsDescriptor, Ownable {
     }
 
     /**
-     * @notice Lock all Noun parts.
-     * @dev This cannot be reversed and can only be called by the owner when not locked.
-     */
-    function lockParts() external override onlyOwner whenPartsNotLocked {
-        arePartsLocked = true;
-
-        emit PartsLocked();
-    }
-
-    /**
-     * @notice Toggle a boolean value which determines if `tokenURI` returns a data URI
-     * or an HTTP URL.
-     * @dev This can only be called by the owner.
-     */
-    function toggleDataURIEnabled() external override onlyOwner {
-        bool enabled = !isDataURIEnabled;
-
-        isDataURIEnabled = enabled;
-        emit DataURIToggled(enabled);
-    }
-
-    /**
-     * @notice Set the base URI for all token IDs. It is automatically
-     * added as a prefix to the value returned in {tokenURI}, or to the
-     * token ID if {tokenURI} is empty.
-     * @dev This can only be called by the owner.
-     */
-    function setBaseURI(string calldata _baseURI) external override onlyOwner {
-        baseURI = _baseURI;
-
-        emit BaseURIUpdated(_baseURI);
-    }
-
-    /**
      * @notice Given a token ID and seed, construct a token URI for an official Nouns DAO noun.
      * @dev The returned value may be a base64 encoded data URI or an API URL.
      */
-    function tokenURI(uint256 tokenId, INounsSeeder.Seed memory seed) external view override returns (string memory) {
-        if (isDataURIEnabled) {
-            return dataURI(tokenId, seed);
-        }
-        return string(abi.encodePacked(baseURI, tokenId.toString()));
+    function tokenURI(uint256 tokenId, INounsSeeder.Seed memory seed) external view returns (string memory) {
+        return dataURI(tokenId, seed);
     }
 
     /**
      * @notice Given a token ID and seed, construct a base64 encoded data URI for an official Nouns DAO noun.
      */
-    function dataURI(uint256 tokenId, INounsSeeder.Seed memory seed) public view override returns (string memory) {
+    function dataURI(uint256 tokenId, INounsSeeder.Seed memory seed) public view returns (string memory) {
         string memory nounId = tokenId.toString();
         string memory name = string(abi.encodePacked('Noun ', nounId));
         string memory description = string(abi.encodePacked('Noun ', nounId, ' is a member of the Nouns DAO'));
@@ -452,7 +251,7 @@ contract FFNDescriptor is INounsDescriptor, Ownable {
         string memory name,
         string memory description,
         INounsSeeder.Seed memory seed
-    ) public view override returns (string memory) {
+    ) public view returns (string memory) {
         NFTDescriptor.TokenURIParams memory params = NFTDescriptor.TokenURIParams({
             name: name,
             description: description,
@@ -466,7 +265,7 @@ contract FFNDescriptor is INounsDescriptor, Ownable {
     /**
      * @notice Given a seed, construct a base64 encoded SVG image.
      */
-    function generateSVGImage(INounsSeeder.Seed memory seed) external view override returns (string memory) {
+    function generateSVGImage(INounsSeeder.Seed memory seed) external view returns (string memory) {
         MultiPartRLEToSVG.SVGParams memory params = MultiPartRLEToSVG.SVGParams({
             parts: _getPartsForSeed(seed),
             // modified
@@ -480,41 +279,6 @@ contract FFNDescriptor is INounsDescriptor, Ownable {
      */
     function _addColorToPalette(uint8 _paletteIndex, string calldata _color) internal {
         palettes[_paletteIndex].push(_color);
-    }
-
-    /**
-     * @notice Add a Noun background.
-     */
-    function _addBackground(string calldata _background) internal {
-        backgrounds.push(_background);
-    }
-
-    /**
-     * @notice Add a Noun body.
-     */
-    function _addBody(bytes calldata _body) internal {
-        bodies.push(_body);
-    }
-
-    /**
-     * @notice Add a Noun accessory.
-     */
-    function _addAccessory(bytes calldata _accessory) internal {
-        accessories.push(_accessory);
-    }
-
-    /**
-     * @notice Add a Noun head.
-     */
-    function _addHead(bytes calldata _head) internal {
-        heads.push(_head);
-    }
-
-    /**
-     * @notice Add Noun glasses.
-     */
-    function _addGlasses(bytes calldata _glasses) internal {
-        glasses.push(_glasses);
     }
 
     /**
