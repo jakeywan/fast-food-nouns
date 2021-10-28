fe2f575b8a344c007bba6c688dd6dd4948e78145
Jake Allen <jake@hintsy.co>
2021-10-28 08:41:13 -0700

    Added wearables minting and contract deployment into deploy script.

--- scripts/hardhatDeploy.js
+++ scripts/hardhatDeploy.js
@@ -4,16 +4,22 @@ async function main() {
   console.log('Deploying contracts with the account:', deployer.address)
   console.log('Account balance:', (await deployer.getBalance()).toString())
 
+  // DEPLOY FFNS
   const NounsToken = await ethers.getContractFactory('FastFoodNouns')
   // Use the deployed descriptor address so NounsToken points to it
   const nounsToken = await NounsToken.deploy();
-
   console.log('FastFoodNouns deployed to: ', nounsToken.address)
 
+  // DEPLOY DESCRIPTOR
   const FFNDescriptor = await ethers.getContractFactory('FFNDescriptor');
   const ffnDescriptor = await FFNDescriptor.deploy();
-
   console.log('FFNDescriptor deployed to: ', ffnDescriptor.address)
+
+  // DEPLOY WEARABLES
+  const Wearables = await ethers.getContractFactory('FFNWearables')
+  // Use the deployed descriptor address so NounsToken points to it
+  const wearablesContract = await Wearables.deploy();
+  console.log('Wearables deployed to: ', wearablesContract.address)
 
   // If we're deploying to mainnet, stop here
   if (process.env.HARDHAT_NETWORK !== 'rinkeby' && process.env.HARDHAT_NETWORK !== 'localhost') return
@@ -352,6 +358,21 @@ async function main() {
     from: testAccounts[0]
   })
   console.log('Token uri for tokenId 0: ', tokenURI)
+
+  // Mint first wearables
+  const wearable = {
+    name: 'Gremplin Stains',
+    rleData: '0x0015171f090e020e02020201030302020406020102030302020204010202050302010201030100010301020104010201040102030502020202010004020104020202050202020201000702010503020202010002020105080202020100040201040602020201000b02020201000b02',
+    palette: ["", "000000", "ede7ce", "ddd1a0", "c15927", "e0c12e"],
+    gridSize: 32
+  }
+  const mintWearable = await wearablesContract.mint(10, wearable, {
+    gasLimit: 550000
+  })
+  console.log('Minted wearables')
+
+  const tokenURIWearable = await wearablesContract.tokenURI(0)
+  console.log('Wearable TokenURI: ', tokenURIWearable);
 
 }
 
