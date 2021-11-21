fdf87901c40b13e641c39ceab49240a08352d1f4
Jake Allen <jake@hintsy.co>
2021-11-21 08:29:45 -0800

    Added svgs and seeds to upload script.

--- scripts/deployArbisNouns.js
+++ scripts/deployArbisNouns.js
@@ -1,14 +1,32 @@
+const { svgData } = require('../files/svgData.js')
+const { seeds } = require('../files/seeds.js')
+
 async function main() {
 
   const [deployer] = await ethers.getSigners();
   console.log('Deploying contracts with the account:', deployer.address)
 
-  //=========================DEPLOY CONTRACTS================================
-
   // DEPLOY ARBIS NOUNS
   const ArbisNouns = await ethers.getContractFactory('ArbisNouns')
   const arbisNouns = await ArbisNouns.deploy()
   console.log('ArbisNouns deployed to: ', arbisNouns.address)
+
+
+  // UPDATE HEAD SVGS
+  for (let i = 0; i < svgData.heads.length; i++) {
+    await arbisNouns.updateHeadSVG(i, svgData.heads[i].innerSVG)
+    console.log('head updated ', svgData.heads[i].innerSVG)
+  }
+
+  // UPDATE SEEDS
+  for (let i = 0; i < seeds.length; i++) {
+    await arbisNouns.updateSeed(seeds[i], i)
+    console.log('seed updated ', i)
+  }
+
+  // UPDATE SNAPSHOT
+
+
 
 }
 
